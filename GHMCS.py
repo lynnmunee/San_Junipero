afb0f0f44edf37ba1538a95bb35705a73dbb9c8c
Ricky Nyairo <rickynyairo@gmail.com>
2018-03-20 16:14:05 +0300

    Making progress

--- GHMCS.py
+++ GHMCS.py
@@ -2,12 +2,19 @@
 import RPi.GPIO as GPIO
 from time import sleep
 import Adafruit_DHT
-#GPIO SETUP
+
+#Pin Setup
 fan_forward = 20
 soil_moisture = 21
+water_pump = 16
+vent_servo = 02
+#GPIO SETUP
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(fan_forward, GPIO.OUT)
+GPIO.setup(water_pump, GPIO.OUT)
 GPIO.setup(soil_moisture, GPIO.IN)
+
+
 #infinite loop
 while True:
 	print "Hello world"
@@ -20,13 +27,24 @@ while True:
 	soil_moisture_state = GPIO.input(soil_moisture)
 
 	if (soil_moisture_state == 1):
-		print "Soil State is 1"
-		GPIO.output(fan_forward, GPIO.HIGH)
+		print "The soil is dry"
+		GPIO.output(water_pump, GPIO.HIGH)
+		sleep(3)
 	else:
-		print "Soil State is 0"
-		GPIO.output(fan_forward, GPIO.LOW)
+		print "The soil is wet"
+		GPIO.output(water_pump, GPIO.LOW)
+
+	if (temperature > 29):
+		print "High Temperature, starting fan"
+		GPIO.output(fan_forward, GPIO.HIGH)
+
+		open_vent()
 
 
 	sleep(2)
 
+#function to open vent
+def open_vent():
+
+	pass
 
