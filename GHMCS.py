439d22c41ab10abcc811489589fa55fd25c48db2
lynnmunee <muneekioko@gmail.com>
2018-03-20 19:32:01 +0300

    LDR

--- GHMCS.py
+++ GHMCS.py
@@ -4,11 +4,13 @@ from time import sleep
 import Adafruit_DHT
 
 #Pin Setup
+ldr=24
 fan_forward = 20
 soil_moisture = 21
 water_pump = 16
 vent_servo = 02
 vent_angle = 90
+
 #GPIO SETUP
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(fan_forward, GPIO.OUT)
@@ -16,17 +18,14 @@ GPIO.setup(water_pump, GPIO.OUT)
 GPIO.setup(soil_moisture, GPIO.IN)
 GPIO.setup(vent_servo, GPIO.OUT)
 
-
-
-
 #infinite loop
 def main_func():
 
 	humidity, temperature = Adafruit_DHT.read_retry(11,4)
 
-	print "Temperature: " + str(temperature) + " C"
-	print "Humidity: " + str(humidity) + " %%"
-
+	print "Temperature: " + str(temperature) + " °C"
+	print "Humidity: " + str(humidity) + " %"
+	print "LDR: " + str(ldr_reading())
 	soil_moisture_state = GPIO.input(soil_moisture)
 
 	if (soil_moisture_state == 1):
@@ -44,6 +43,21 @@ def main_func():
 		open_vent(vent_angle)
 
 	sleep(2)
+
+#function for the ldr
+def ldr_reading():
+	count=0
+
+	GPIO.setup(ldr, GPIO.OUT)
+	GPIO.output(ldr, GPIO.LOW)
+	time.sleep(1)
+
+	GPIO.setup(ldr,GPIO.IN)
+
+	while(GPIO.input(ldr)==GPIO.LOW):
+		count += 1
+
+	return count
 
 #function to open vent
 def open_vent(angle = vent_angle):
